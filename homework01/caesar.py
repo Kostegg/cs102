49424cf88ab443296594b583f5465480cc8e994e
Kostegg <Kostik20001@gmail.com>
2020-10-10 13:52:36 +0300

    Убран лишний код и переименована функция взломщика обратно в caesar_breaker

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -2,18 +2,6 @@ import typing as tp
 
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
     ciphertext = ""
     for c in plaintext:
         if c.isalpha():
@@ -27,18 +15,6 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
     plaintext = ""
     for c in ciphertext:
         if c.isalpha():
@@ -52,14 +28,6 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
 
 
 def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    >>> d = {"python", "java", "ruby"}
-    >>> caesar_breaker_brute_force("python", d)
-    0
-    >>> caesar_breaker_brute_force("sbwkrq", d)
-    3
-    """
     best_shift = 0
     plaintext = ""
     while best_shift < 26:
